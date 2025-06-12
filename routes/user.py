e2eb664549b8e02c0d787cbf3ddc9273b648302e
rkanani <reyhanehkanani4@gmail.com>
2025-06-12 11:28:20 -0700

    creat class order
    calss order item ( vijegi kalayi keh sefaresh dadeh )
    class order vaziyat sefaresh va marahelo...

--- routes/user.py
+++ routes/user.py
@@ -29,12 +29,21 @@ class provider(SQLModel, table=True):
     active: bool = True #vaziyzat tamin konandeh keh ghabel dastres ast ya nist
 
 
-class order(SQLModel, table=True):
-    id: Optional[int] = Field(default=None, primary_key=True)  
-    name: str
-    price: float
-    quantity: int = 0
-    category: Optional[str] = None
+class Order(SQLModel, table=True): 
+    id: Optional[int] = Field(default=None, primary_key=True) 
+    order_type: OrderType  # "purchase" or "sales" 
+    provider_id: Optional[int] = Field(default=None, foreign_key="provider.id") 
+    customer_name: Optional[str] = None 
+    status: str  # Draft, Sent, Received, Closed, Pending, Completed, ... 
+    order_date: datetime = Field(default_factory=datetime.utcnow) 
+ 
+class OrderItem(SQLModel, table=True): 
+    id: Optional[int] = Field(default=None, primary_key=True) 
+    order_id: int = Field(foreign_key="order.id") 
+    product_id: int = Field(foreign_key="product.id") 
+    quantity: int 
+    unit_price: float
+ 
     
         
      
