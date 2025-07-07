114ef05780a5f83bb3aa2e0b84cce71a6abccc95
sarakhatibi <khatibi.sara85@gmail.com>
2025-07-07 12:39:52 +0330

    Create delete.py
    با استفاده از دیلیت در این قسمت اگر خواستیم سفارشی حذف بشه از این قسمت استفاده میکنیم

---
+++ routes/order/delete.py
@@ -0,0 +1,11 @@
+
+@router.delete("/orders/{order_id}")
+def delete_order(order_id: int, session: Session = Depends(get_session)):
+    order = session.get(Order, order_id)
+    if not order:
+        raise HTTPException(status_code=404, detail="Order not found")
+
+    session.delete(order)
+    session.commit()
+
+    return {"message": "Order deleted successfully."}
