2b566879597702d776788d210cacd5efecb6bed7
SedighehYazdani <sevda.1385.ir@gmail.com>
2025-07-07 13:35:38 +0330

    Create jwt.py
     احراز هویت کاربر با استفاده از JWT

---
+++ security/jwt.py
@@ -0,0 +1,13 @@
+from typing import Optional
+from jose import jwt, JWTError
+from datetime import datetime, timedelta
+
+SECRET_KEY = "mysecretkey"
+ALGORITHM = "HS256"
+ACCESS_TOKEN_EXPIRE_MINUTES = 30
+
+def create_access_token(data: dict, expires_delta: Optional[timedelta] = None):
+    to_encode = data.copy()
+    expire = datetime.utcnow() + (expires_delta or timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES))
+    to_encode.update({"exp": expire})
+    return jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
