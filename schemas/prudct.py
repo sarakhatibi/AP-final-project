1a66e2acd68eff568db022f77dea60fbebef9e03
SedighehYazdani <sevda.1385.ir@gmail.com>
2025-07-07 10:39:15 +0330

    و کلاس های پروداکت schemasایجاد پوشه ی
    در اینجا سه کلاس جدید برای پروداکت ساختیم
    1product creat
     برای ساخت محصول جدید
    2 product read
    برای نمایش اطلاعات محصول به کلاینت
    3productupdate
    مدل برای به روزرسانی محصول

---
+++ schemas/prudct.py
@@ -0,0 +1,31 @@
+from typing import Optional
+from sqlmodel import SQLModel, Field
+
+
+class ProductCreate(SQLModel):
+    name: str
+    sku: str
+    price: float
+    min_threshold: int
+    category: Optional[str] = None
+    image: Optional[str] = None
+    quantity: int = 0
+
+class ProductRead(SQLModel):
+    id: int
+    name: str
+    sku: str
+    price: float
+    min_threshold: int
+    category: Optional[str] = None
+    image: Optional[str] = None
+    quantity: int
+
+class ProductUpdate(SQLModel):
+    name: Optional[str] = None
+    sku: Optional[str] = None
+    price: Optional[float] = None
+    min_threshold: Optional[int] = None
+    category: Optional[str] = None
+    image: Optional[str] = None
+    quantity: Optional[int] = None
