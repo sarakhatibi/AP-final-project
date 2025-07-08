52e21a6825a2f4c447fd4c0489561dc872e1f505
rkanani <reyhanehkanani4@gmail.com>
2025-07-07 17:54:42 -0700

    Connecting the providres section to main

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 from routes.order import router as order_router
+from routes.provider import router as provider_router
 
 app=FastAPI()
 
@@ -8,3 +9,4 @@ def read_root():
     return{"message":"Welcome to our shop!"}
 
 app.include_router(order_router, tags=["Orders"])
+app.include_router(provider_router, tags=["providers"])
---
+++ routes/provider/__init__.py
@@ -0,0 +1,12 @@
+from fastapi import APIRouter
+from addprovider import router as addprovider 
+from delete import router as delete
+from display import router as display
+from edit import router as edit
+
+router = APIRouter(prefix="/providers")
+
+router.include_router(addprovider)
+router.include_router(delete)
+router.include_router(display)
+router.include_router(edit)
