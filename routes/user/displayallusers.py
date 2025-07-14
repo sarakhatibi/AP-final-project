c92cbd34bb7560c05e519a8fce5858cc517408ae
SedighehYazdani <sevda.1385.ir@gmail.com>
2025-07-15 00:19:10 +0330

    get current user in displaying all users

--- routes/user/displayallusers.py
+++ routes/user/displayallusers.py
@@ -3,6 +3,7 @@ from sqlmodel import Session, select
 from database.connection import get_session
 from model.user import User
 from schemas.user import UserRead
+from security.auth import get_current_user
 
 router = APIRouter(tags=["Users"])
 
@@ -14,8 +15,11 @@ def get_db():
 def get_all_users(
     page: int = Query(1, ge=1),
     limit: int = Query(10, ge=1),
-    session: Session = Depends(get_db)
+    session: Session = Depends(get_db),
+    current_user: User = Depends(get_current_user)
 ):
+    if current_user.role != "admin":
+        raise HTTPException(status_code=403, detail="Access denied. Only admins can see all users.")
     offset = (page - 1) * limit
     query = select(User).offset(offset).limit(limit)
     users = session.exec(query).all()
