96da14b0b39feeb19de651c8869043af31549590
SedighehYazdani <sevda.1385.ir@gmail.com>
2025-07-15 00:57:06 +0330

    Increase inventory when goods arrive and update information about a product if the SKU code already exists.

--- routes/product/add_product.py
+++ routes/product/add_product.py
@@ -20,8 +20,23 @@ def get_db():
 def create_product(product: ProductCreate, session: Session = Depends(get_db),current_user: User = Depends(get_current_user)): 
     if current_user.role != "admin":
         raise HTTPException(status_code=403, detail="Access denied. Only admins can create products.")
-    db_product = Product.from_orm(product) 
-    session.add(db_product) 
-    session.commit() 
-    session.refresh(db_product) 
-    return db_product
+    existing_product = session.exec(select(Product).where(Product.sku == product.sku)).first()
+    
+    if existing_product:
+        existing_product.quantity += product.quantity
+
+        updated_fields = product.dict(exclude_unset=True)
+        for key, value in updated_fields.items():
+            if key != "quantity": 
+                setattr(existing_product, key, value)
+
+        session.add(existing_product)
+        session.commit()
+        session.refresh(existing_product)
+        return existing_product
+    else:
+        db_product = Product.from_orm(product)
+        session.add(db_product)
+        session.commit()
+        session.refresh(db_product)
+        return db_product
