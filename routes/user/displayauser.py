573099ce090b3cfdceb6583f308ac4746e813d30
SedighehYazdani <sevda.1385.ir@gmail.com>
2025-07-15 00:17:38 +0330

    Update displayauser.py
    get current user in display all users

--- routes/user/displayauser.py
+++ routes/user/displayauser.py
@@ -3,6 +3,7 @@ from sqlmodel import Session, select
 from database.connection import get_session
 from model.user import User
 from schemas.user import UserRead, UserCreate, UserUpdate, UserLogin
+from security.auth import get_current_user
 
 def get_db(): 
     with get_session() as session: 
@@ -10,7 +11,9 @@ def get_db():
 
 router = APIRouter (tags=["Users"])
 @router.get("/users/{user_id}", response_model=UserRead)
-def get_single_user(user_id: int, session: Session = Depends(get_db)):
+def get_single_user(user_id: int, session: Session = Depends(get_db),current_user: User = Depends(get_current_user)):
+ if current_user.role != "admin":
+        raise HTTPException(status_code=403, detail="Access denied. Only admins can see a special user.")
  user = session.get(User, user_id)
  if not user:
         raise HTTPException(
