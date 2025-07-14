0a13a1e90a9346e1e6013a8153f2700635068567
SedighehYazdani <sevda.1385.ir@gmail.com>
2025-07-15 00:26:42 +0330

    limitation for user in deleting providers

--- routes/provider/delete.py
+++ routes/provider/delete.py
@@ -3,6 +3,9 @@ from fastapi import APIRouter, Depends, HTTPException, status
 from sqlmodel import Session, select
 from database.connection import get_session
 from model.provider import provider
+from security.auth import get_current_user
+from model.user import User
+
 
 router = APIRouter()
 
@@ -11,7 +14,9 @@ def get_db():
         yield session
 
 @router.delete("/providers/{provider_id}", status_code=status.HTTP_204_NO_CONTENT)
-def delete_provider(provider_id: int, session: Session = Depends(get_session)):
+def delete_provider(provider_id: int, session: Session = Depends(get_session),current_user: User = Depends(get_current_user)):
+    if current_user.role != "admin":
+        raise HTTPException(status_code=403, detail="Access denied. Only admins can delete providers.")
     provider = session.get(provider, provider_id)
     if not provider:
         raise HTTPException(status_code=404, detail="Provider not found")
