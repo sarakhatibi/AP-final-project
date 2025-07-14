4f76e6adae366e1c00bbc39c896cda79b112d119
SedighehYazdani <sevda.1385.ir@gmail.com>
2025-07-15 00:25:16 +0330

    limitation for user in adding provider

--- routes/provider/addprovider.py
+++ routes/provider/addprovider.py
@@ -4,11 +4,15 @@ from sqlmodel import Session
 from database.connection import get_session
 from model.provider import provider
 from schemas.provider import ProviderCreate
+from security.auth import get_current_user
+from model.user import User
 
 router = APIRouter()
 
 @router.post("/providers/", response_model=provider)
-def create_provider(provider: ProviderCreate, session: Session = Depends(get_session)):
+def create_provider(provider: ProviderCreate, session: Session = Depends(get_session),current_user: User = Depends(get_current_user)):
+    if current_user.role != "admin":
+        raise HTTPException(status_code=403, detail="Access denied. Only admins can add providers.")
     try:
     
         db_provider = provider.from_orm(provider)
