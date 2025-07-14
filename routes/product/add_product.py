29b8e3278ce00935ad0d36e0dd9d09d2c1bef93a
SedighehYazdani <sevda.1385.ir@gmail.com>
2025-07-14 23:56:06 +0330

    get current user in add-product

--- routes/product/add_product.py
+++ routes/product/add_product.py
@@ -5,6 +5,8 @@ from datetime import datetime
 from model.product import Product
 from schemas.product import ProductCreate, ProductRead, ProductUpdate 
 from database.connection import get_session 
+from security.auth import get_current_user
+from model.user import User
  
 router = APIRouter() 
  
@@ -15,7 +17,9 @@ def get_db():
  
 
 @router.post("/", response_model=ProductRead) 
-def create_product(product: ProductCreate, session: Session = Depends(get_db)): 
+def create_product(product: ProductCreate, session: Session = Depends(get_db),current_user: User = Depends(get_current_user)): 
+    if current_user.role != "admin":
+        raise HTTPException(status_code=403, detail="Access denied. Only admins can create products.")
     db_product = Product.from_orm(product) 
     session.add(db_product) 
     session.commit() 
