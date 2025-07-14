bb11da3404699bf9b1911b574aa832f3a6b2af0b
SedighehYazdani <sevda.1385.ir@gmail.com>
2025-07-14 23:45:39 +0330

    Add get current user in delete in product

--- routes/product/delete.py
+++ routes/product/delete.py
@@ -6,13 +6,18 @@ from datetime import datetime
 from model.product import Product
 from schemas.product import ProductCreate, ProductRead, ProductUpdate 
 from database.connection import get_session  
+from security.auth import get_current_user
+from model.user import User
 router = APIRouter() 
 def get_db(): 
     with get_session() as session: 
         yield session 
 
 @router.delete("/{product_id}") 
-def delete_product(product_id: int, session: Session = Depends(get_db)): 
+def delete_product(product_id: int, session: Session = Depends(get_db),current_user: User = Depends(get_current_user)): 
+    if current_user.role != "admin":
+        raise HTTPException(status_code=403, detail="Access denied. Only admins can delete products.")
+
     product = session.get(Product, product_id) 
     if not product: 
         raise HTTPException(status_code=404, detail="Product not found") 
