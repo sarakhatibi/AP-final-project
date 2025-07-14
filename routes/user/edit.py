000fc0363c26c1cfe680902a7276e8e12c1d9686
SedighehYazdani <sevda.1385.ir@gmail.com>
2025-07-15 00:18:21 +0330

    get current user in editing user

--- routes/user/edit.py
+++ routes/user/edit.py
@@ -3,6 +3,7 @@ from sqlmodel import Session
 from database.connection import get_session
 from model.user import User
 from schemas.user import UserUpdate   
+from security.auth import get_current_user
 
 router = APIRouter()
 
@@ -11,16 +12,18 @@ def get_db():
         yield session
 
 @router.put("/users/{user_id}")
-def update_user(user_id: int, user_update: UserUpdate, session: Session = Depends(get_db)):
-    user = session.get(User, user_id)
-    if not user:
+def update_user(user_id: int, user_update: UserUpdate, session: Session = Depends(get_db),current_user: User = Depends(get_current_user)):
+ if current_user.role != "admin":
+        raise HTTPException(status_code=403, detail="Access denied. Only admins can edit users.")
+ user = session.get(User, user_id)
+ if not user:
         raise HTTPException(status_code=404, detail="User not found")
     
-    user.full_name = user_update.full_name
-    user.email = user_update.email
-    user.username = user_update.username
-    user.password=user_update.password
-    session.add(user)
-    session.commit()
-    session.refresh(user)
-    return {"msg": "User updated successfully", "user": user}
+ user.full_name = user_update.full_name
+ user.email = user_update.email
+ user.username = user_update.username
+ user.password=user_update.password
+ session.add(user)
+ session.commit()
+ session.refresh(user)
+ return {"msg": "User updated successfully", "user": user}
