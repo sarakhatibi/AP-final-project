423980a899ff6350cb82493d55302324cdef6aa9
SedighehYazdani <sevda.1385.ir@gmail.com>
2025-07-15 00:19:37 +0330

    get current user in deleting users

--- routes/user/delete.py
+++ routes/user/delete.py
@@ -2,6 +2,7 @@ from fastapi import APIRouter, Depends, HTTPException, status
 from sqlmodel import Session
 from database.connection import get_session
 from model.user import User  
+from security.auth import get_current_user
 
 router = APIRouter()
 
@@ -10,7 +11,10 @@ def get_db():
         yield session
 
 @router.delete("/users/{user_id}", status_code=status.HTTP_204_NO_CONTENT)
-def delete_user(user_id: int, session: Session = Depends(get_db)):
+def delete_user(user_id: int, session: Session = Depends(get_db),current_user: User = Depends(get_current_user)):
+    if current_user.role != "admin":
+        raise HTTPException(status_code=403, detail="Access denied. Only admins can delete users.")
+
     user = session.get(User, user_id)
     if not user:
         raise HTTPException(status_code=404, detail="User not found")
