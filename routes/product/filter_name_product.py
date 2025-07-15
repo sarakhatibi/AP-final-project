068ec62628da88a870e1962fa41ca3be67e3411a
rkanani <reyhanehkanani4@gmail.com>
2025-07-15 00:04:16 -0700

    limit&page(filter  by name)
    صحفه بندی با پارت و لیمیت

--- routes/product/filter_name_product.py
+++ routes/product/filter_name_product.py
@@ -1,20 +1,39 @@
-from fastapi import APIRouter, Depends, HTTPException 
-from sqlmodel import Session, select 
-from typing import List, Optional 
-from datetime import datetime 
- 
+from fastapi import APIRouter, Depends, HTTPException, Query
+from sqlmodel import Session, select
+from typing import List, Optional
 from model.product import Product
-from schemas.product import ProductCreate, ProductRead, ProductUpdate 
-from database.connection import get_session 
- 
-router = APIRouter() 
-def get_db(): 
-    with get_session() as session: 
+from schemas.product import ProductRead
+from database.connection import get_session
+
+router = APIRouter()
+
+def get_db():
+    with get_session() as session:
         yield session
-@router.get("/", response_model=List[ProductRead]) 
-def read_products(skip: int = 0, limit: int = 100, name: Optional[str] = None, session: Session = Depends(get_db)): 
-    query = select(Product) 
-    if name: 
-        query = query.where(Product.name.contains(name)) 
-    products = session.exec(query.offset(skip).limit(limit)).all() 
+
+@router.get("/", response_model=List[ProductRead])
+def read_products(
+    page: int = Query(1, ge=1),
+    limit: int = Query(10, ge=1),
+    name: Optional[str] = None,
+    session: Session = Depends(get_db)
+):
+   
+    if page < 1 or limit < 1:
+        raise HTTPException(status_code=400, detail="page و limit باید عددی بزرگ‌تر از صفر باشند.")
+
+   
+    offset = (page - 1) * limit
+
+    query = select(Product)
+    if name:
+        query = query.where(Product.name.contains(name))
+
+    products = session.exec(query.offset(offset).limit(limit)).all()
+
+    
+    if not products:
+        raise HTTPException(status_code=404, detail="هیچ محصولی در این صفحه پیدا نشد.")
+
+    
     return products
