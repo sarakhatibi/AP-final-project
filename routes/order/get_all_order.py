7ceab8fe56812dec445cc3658f7f40ccc17a38c6
rkanani <reyhanehkanani4@gmail.com>
2025-07-14 23:56:01 -0700

    part _limit (get all order)
    صحفه بندی با پارت و لیمیت

--- routes/order/get_all_order.py
+++ routes/order/get_all_order.py
@@ -1,13 +1,24 @@
-from fastapi import APIRouter, Depends, HTTPException
+from fastapi import APIRouter, Depends, HTTPException, Query
 from sqlmodel import Session, select
 from model.order import Order
-from schemas.order import OrderCreate, OrderRead, OrderUpdate
+from schemas.order import OrderRead
 from database.connection import get_session
 
 router = APIRouter()
 
 @router.get("/orders/", response_model=list[OrderRead])
-def get_all_orders(session: Session = Depends(get_session)):
-    orders = session.exec(select(Order)).all()
+def get_all_orders(
+    page: int = Query(1, ge=1),
+    limit: int = Query(10, ge=1),
+    session: Session = Depends(get_session)
+):
+    offset = (page - 1) * limit
+    query = select(Order).offset(offset).limit(limit)
+
+    orders = session.exec(query).all()
+
+    if not orders:
+        raise HTTPException(status_code=404, detail="سفارشی در این صفحه پیدا نشد.")
+
     return orders
 
