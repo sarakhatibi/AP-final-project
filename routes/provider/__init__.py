e4e97f6fb4c0e3448219c9a94419bae4386b21f7
rkanani <reyhanehkanani4@gmail.com>
2025-07-17 16:19:03 -0700

    creat function for showing a provider by id
    تابعی برای نمایش اطلاعات یک تامین کننده

--- routes/provider/__init__.py
+++ routes/provider/__init__.py
@@ -3,6 +3,7 @@ from .addprovider import router as addprovider_router
 from .delete import router as delete_router
 from .display import router as display_router
 from .edit import router as edit_router
+from .showbyid import router as showbyid_router
 
 router = APIRouter()
 
@@ -10,3 +11,4 @@ router.include_router(addprovider_router, prefix="", tags=["Providers - Add"])
 router.include_router(delete_router, prefix="", tags=["Providers - Delete"])
 router.include_router(display_router, prefix="", tags=["Providers - Display"])
 router.include_router(edit_router, prefix="", tags=["Providers - Edit"])
+router.include_router(showbyid_router, prefix="", tags=["showbyid - Add"])
---
+++ routes/provider/showbyid.py
@@ -0,0 +1,26 @@
+
+from fastapi import APIRouter, Depends, HTTPException
+from sqlmodel import Session
+from database.connection import get_session
+from model.provider import provider
+from security.auth import get_current_user
+from model.user import User
+from schemas.provider import ProviderRead
+
+router = APIRouter()
+
+@router.get("/showbyid/{provider_id}", response_model=ProviderRead)
+def read_single_provider(
+    provider_id: int,
+    session: Session = Depends(get_session),
+    current_user: User = Depends(get_current_user)
+):
+    if current_user.role != "admin":
+        raise HTTPException(status_code=403, detail="دسترسی غیرمجاز. فقط ادمین‌ها می‌توانند اطلاعات تأمین‌کننده را ببینند.")
+    
+    provider_data = session.get(provider, provider_id)
+
+    if not provider_data:
+        raise HTTPException(status_code=404, detail="تأمین‌کننده‌ای با این شناسه یافت نشد.")
+    
+    return provider_data
