5ce804575abbd9a345d42666b3119c3d8e26fa76
sarakhatibi <khatibi.sara85@gmail.com>
2025-07-18 16:33:42 +0330

    debug(users)
    چک کردن قسمت ادیت و نمایش تمام کاربرها و همچنین گرفتن خطاهای 500 در قسمت ادیت و ویرایش قسمت ادیت

--- routes/user/edit.py
+++ routes/user/edit.py
@@ -4,23 +4,35 @@ from database.connection import get_db
 from model.user import User
 from schemas.user import UserUpdate   
 from security.auth import get_current_user
+from security.hash import hash_password
 
 router = APIRouter()
 
-
 @router.put("/edit/{user_id}")
-def update_user(user_id: int, user_update: UserUpdate, session: Session = Depends(get_db),current_user: User = Depends(get_current_user)):
- if current_user.role != "admin":
+def update_user(
+    user_id: int,
+    user_update: UserUpdate,
+    session: Session = Depends(get_db),
+    current_user: User = Depends(get_current_user)
+):
+    if current_user.role != "admin":
         raise HTTPException(status_code=403, detail="Access denied. Only admins can edit users.")
- user = session.get(User, user_id)
- if not user:
+
+    user = session.get(User, user_id)
+    if not user:
         raise HTTPException(status_code=404, detail="User not found")
-    
- user.full_name = user_update.full_name
- user.email = user_update.email
- user.username = user_update.username
- user.hashed_password=user_update.password
- session.add(user)
- session.commit()
- session.refresh(user)
- return {"msg": "User updated successfully", "user": user}
+
+    if user_update.full_name is not None:
+        user.full_name = user_update.full_name
+    if user_update.email is not None:
+        user.email = user_update.email
+    if user_update.username is not None:
+        user.username = user_update.username
+    if user_update.password is not None and user_update.password.strip() != "":
+        user.hashed_password = hash_password(user_update.password)
+
+    session.add(user)
+    session.commit()
+    session.refresh(user)
+
+    return {"msg": "User updated successfully", "user": user}
