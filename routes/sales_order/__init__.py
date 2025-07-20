c7cc3b8bb5135c63e2461dfe841d8d29288d223b
rkanani <reyhanehkanani4@gmail.com>
2025-07-19 23:06:43 -0700

    edit status of order
    ساخت  تابعی برای تغییر وضعیت سفارش کاربر
    و وصل کردن ان به مین

--- routes/sales_order/__init__.py
+++ routes/sales_order/__init__.py
@@ -1,6 +1,8 @@
 from fastapi import APIRouter
 from .add import router as add_router
+from .status import router as status_router
 
 
 router = APIRouter()
 router.include_router(add_router, prefix="", tags=["Orders - Add"])
+router.include_router(status_router, prefix="", tags=["Status - Add"])
